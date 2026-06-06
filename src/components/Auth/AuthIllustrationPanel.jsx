f2bd520a93ba15c095bed5672a5e8a48fddacb02
Ahmed Samir <ahmedsamir9828@gmail.com>
2026-06-06 06:29:08 +0300

    edit on pictures of doctors and dark theme

--- src/components/Auth/AuthIllustrationPanel.jsx
+++ src/components/Auth/AuthIllustrationPanel.jsx
@@ -1,40 +1,68 @@
 import { Link } from "react-router-dom";
-import doctorImage from "../../assets/Auth/signInDoctor.png";
+import doctorImage from "../../assets/landingPage/signupDoctor.png";
+import doctorAddsDark from "../../assets/landingPage/signup-layOut.png";
+import doctorAddsLight from "../../assets/landingPage/signup-layOut-light.png";
+import bgDark from "../../assets/landingPage/login-bg-dark.png";
+import bgLight from "../../assets/landingPage/login-bg-light.png";
+import { useTheme } from "../../hooks/useTheme";
+import union from "../../assets/landingPage/Union.png";
+import vector from "../../assets/landingPage/Vector 94.png";
 
 export default function AuthIllustrationPanel() {
+  const { dark } = useTheme();
   return (
-    <section className="relative flex  w-1/2 items-center justify-center overflow-hidden rounded-r-[3rem] bg-base-200">
-<Link
-  to="/"
-  className="btn btn-circle btn-sm absolute left-8 top-8 z-40 border-none bg-white text-[#05ADE8] shadow-sm hover:bg-white"
-  aria-label="Back to home"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={2}
-    stroke="currentColor"
-    className="h-5 w-5"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-    />
-  </svg>
-</Link>
+    <section className="relative flex  w-1/2 items-center justify-center overflow-hidden rounded-r-[3rem] bg-(--bg-primary)">
+      <Link
+        to="/"
+        className="btn btn-circle btn-sm absolute left-8 top-8 z-40 border-none bg-white text-[#05ADE8] shadow-sm hover:bg-white"
+        aria-label="Back to home"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="h-5 w-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+      </Link>
       {/* Illustration */}
-      <div className="relative h-[560px] w-[480px]">
+      <div className="relative h-full w-full">
+        <img
+          src={dark ? bgDark : bgLight}
+          alt="Doctor"
+          className="absolute bottom-0 right-0 z-17  w-full h-full object-cover"
+        />
+        <img
+          src={union}
+          alt="Doctor"
+          className="absolute bottom-90 right-10 z-19  object-contain"
+        />
+        <img
+          src={vector}
+          alt="Doctor"
+          className="absolute bottom-40 right-10 z-18  object-contain"
+        />
+        <img
+          src={dark ? doctorAddsDark : doctorAddsLight}
+          alt="Doctor"
+          className="absolute bottom-30 right-0 z-21  w-full h-full object-contain"
+        />
         <img
           src={doctorImage}
           alt="Doctor"
-          className="absolute bottom-6 right-6 z-20 w-[390px] object-contain"
+          className="absolute bottom-10 right-0 z-20  w-full h-full object-contain"
         />
       </div>
 
       {/* Copyright */}
-      <p className="absolute bottom-12 text-xs text-gray-700">
+      <p className="absolute bottom-12 text-xs z-22 text-(--text-primary)">
         حقوق النشر محفوظة 2025-2026 © ميدلينك
       </p>
     </section>
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -2,35 +2,9 @@ import { useEffect, useState } from "react";
 import { ThemeContext } from "./themeContextValue";
 
 export function ThemeProvider({ children }) {
-  const [dark, setDark] = useState(false);
-
-  //   useEffect(() => {
-  //     const savedTheme = localStorage.getItem("theme");
-
-  //     if (savedTheme) {
-  //       const isDark = savedTheme === "dark";
-
-  //       setDark(isDark);
-
-  //       if (isDark) {
-  //         document.documentElement.classList.add("dark");
-  //       } else {
-  //         document.documentElement.classList.remove("dark");
-  //       }
-  //     } else {
-  //       const media = window.matchMedia("(prefers-color-scheme: dark)");
-
-  //       const isDark = media.matches;
-
-  //       setDark(isDark);
-
-  //       if (isDark) {
-  //         document.documentElement.classList.add("dark");
-  //       } else {
-  //         document.documentElement.classList.remove("dark");
-  //       }
-  //     }
-  //   }, []);
+  const [dark, setDark] = useState(
+    () => window.matchMedia("(prefers-color-scheme: dark)").matches,
+  );
 
   useEffect(() => {
     const media = window.matchMedia("(prefers-color-scheme: dark)");
@@ -49,13 +23,6 @@ export function ThemeProvider({ children }) {
       }
     };
 
-    const savedTheme = localStorage.getItem("theme");
-
-    if (savedTheme) {
-      applyTheme(savedTheme === "dark");
-      return;
-    }
-
     applyTheme(media.matches);
 
     const handleChange = (e) => {
@@ -70,21 +37,22 @@ export function ThemeProvider({ children }) {
   }, []);
 
   const toggleTheme = () => {
-    const newDark = !dark;
+    setDark((currentDark) => {
+      const newDark = !currentDark;
 
-    setDark(newDark);
+      document.documentElement.setAttribute(
+        "data-theme",
+        newDark ? "dark" : "light",
+      );
 
-    if (newDark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
+      if (newDark) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
 
-    document.documentElement.setAttribute(
-      "data-theme",
-      newDark ? "dark" : "light",
-    );
-    localStorage.setItem("theme", newDark ? "dark" : "light");
+      return newDark;
+    });
   };
 
   return (
